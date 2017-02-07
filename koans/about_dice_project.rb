62e386ded7465e75d7d3366f41bc65b9b47b1d5d
Chris Vaughan <vaughandroid@gmail.com>
2017-02-07 21:33:57 +0000

    216/282

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def roll(times)
+    @values = []
+    times.times do |i|
+      @values << 1 + rand(6)
+    end
+    @values
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
